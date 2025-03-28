df239281875abd498b385322e2f65ccb77f803c0
Maksim Nasledov <m.nasledov@bbserv.ru>
2025-03-28 15:33:29 +0300

    1. Added common DataProvider. 2. Moved data to the Models.

--- lib/ui/widgets/home_screen/home_page.dart
+++ lib/ui/widgets/home_screen/home_page.dart
@@ -3,7 +3,6 @@ import 'package:web/web.dart' as web;
 import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/material.dart';
-import 'package:flutter_itsolutions_task/services/js_interop_service.dart';
 
 /// [Widget] displaying the home page consisting of an image the buttons.
 class HomePage extends StatefulWidget {
@@ -15,159 +14,15 @@ class HomePage extends StatefulWidget {
 
 /// State of a [HomePage].
 class _HomePageState extends State<HomePage> {
-  final model = HtmlWidgetModel();
-
-  /// Wether the Fullscreen mode is ON.
-  bool _isFullScreen = false;
-
-  /// Wether the PopUp menu is appear.
-  bool _isPopUpMenuOpen = false;
-
-  /// Toggles full screen mode when double clicking on an image.
-  void _onDoubleTapImage() {
-    _toggleFullScreen();
-  }
-
-  /// Toggles the screen between full screen and windowed modes.
-  void _toggleFullScreen() {
-    !_isFullScreen
-        ? JSInteropService.requestFullScreen()
-        : JSInteropService.exitFullScreen();
-
-    _isFullScreen = !_isFullScreen;
-  }
-
-  /// Sets the screen to full screen mode.
-  void _requestFullscreen() {
-    if (_isFullScreen) return;
-    _toggleFullScreen();
-  }
-
-  /// Exit from full screen mode.
-  void _exitFullscreen() {
-    if (!_isFullScreen) return;
-    _toggleFullScreen();
-  }
-
-  /// Updates the state when PopUp menu appears or disappears.
-  void _onToggelePopUpMenu() {
-    setState(() {
-      _isPopUpMenuOpen = !_isPopUpMenuOpen;
-    });
-  }
+  final screenStateModel = ScreenStateModel();
+  final htmlWidgetModel = HtmlWidgetModel();
+  final popUpMenuModel = PopUpMenuModel();
+  late Function() _listener;
 
   @override
-  Widget build(BuildContext context) {
-    List<MenuItemButton> itemButtons = <MenuItemButton>[
-      MenuItemButton(
-          onPressed: _requestFullscreen, child: const Text('Enter fullscreen')),
-      MenuItemButton(
-          onPressed: _exitFullscreen, child: const Text('Exit fullscreen')),
-    ];
+  void initState() {
+    super.initState();
 
-    return Stack(children: [
-      Scaffold(
-        appBar: AppBar(),
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-          child: HtmlWidgetDataProvider(
-            model: model,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                HtmlImageWidget(onDoubleTapImage: _onDoubleTapImage),
-                const SizedBox(height: 8),
-                const _UrlStringWidget(),
-                const SizedBox(height: 64),
-              ],
-            ),
-          ),
-        ),
-        floatingActionButton: _PopUpMenuButton(
-          itemButtons: itemButtons,
-          icon: const Icon(Icons.add),
-          tooltip: 'Select screen mode',
-          onOpen: _onToggelePopUpMenu,
-          onClose: _onToggelePopUpMenu,
-        ),
-      ),
-      _isPopUpMenuOpen ? const OpacityModalWidget() : const SizedBox.shrink(),
-    ]);
-  }
-}
-
-/// A widget used to make PopUp menu button with list of items.
-class _PopUpMenuButton extends StatelessWidget {
-  /// Creates a const [_PopUpMenuButton].
-  ///
-  /// The [itemButtons] argument is required.
-  const _PopUpMenuButton({
-    required this.itemButtons,
-    required this.icon,
-    this.tooltip,
-    this.onOpen,
-    this.onClose,
-    super.key,
-  });
-
-  /// A list of children containing the menu items.
-  final List<Widget> itemButtons;
-
-  /// A callback that is invoked when the menu is opened.
-  final void Function()? onOpen;
-
-  /// A callback that is invoked when the menu is closed.
-  final void Function()? onClose;
-
-  /// The icon to display inside the button.
-  final Widget icon;
-
-  /// Text that describes the action that will occur when the button is pressed.
-  final String? tooltip;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      alignment: AlignmentDirectional.bottomEnd,
-      child: MenuAnchor(
-        builder:
-            (BuildContext context, MenuController controller, Widget? child) {
-          return IconButton(
-            onPressed: () {
-              controller.isOpen ? controller.close() : controller.open();
-            },
-            icon: icon,
-            tooltip: tooltip,
-          );
-        },
-        menuChildren: itemButtons,
-        alignmentOffset: const Offset(-80.0, 15.0),
-        onOpen: onOpen,
-        onClose: onClose,
-      ),
-    );
-  }
-}
-
-/// A widget used to make Image from HTML, using tag <img>.
-class HtmlImageWidget extends StatelessWidget {
-  /// Creates a widget that builds Flutter widget tree from html.
-  ///
-  /// The [imgUrl] and [onDoubleTapImage] arguments must not be null.
-  HtmlImageWidget({
-    required this.onDoubleTapImage,
-    super.key,
-  }) {
-    _registerImgFactory();
-  }
-
-  /// A callback that is invoked when the image is tapped.
-  final void Function() onDoubleTapImage;
-
-  /// Register image view type
-  void _registerImgFactory() {
     ui_web.platformViewRegistry.registerViewFactory(
       'img-view',
       (int viewId, {Object? params}) {
@@ -180,26 +35,125 @@ class HtmlImageWidget extends StatelessWidget {
         return myImg;
       },
     );
+
+    _listener = () => setState(() {});
+    popUpMenuModel.addListener(_listener);
   }
 
   @override
   Widget build(BuildContext context) {
-    final model = HtmlWidgetDataProvider.watch(context);
+    return DataProvider<ScreenStateModel>(
+      model: screenStateModel,
+      child: Stack(children: [
+        Scaffold(
+          appBar: AppBar(),
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+            child: DataProvider<HtmlWidgetModel>(
+              model: htmlWidgetModel,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  HtmlImageWidget(),
+                  SizedBox(height: 8),
+                  _UrlStringWidget(),
+                  SizedBox(height: 64),
+                ],
+              ),
+            ),
+          ),
+          floatingActionButton: DataProvider<PopUpMenuModel>(
+            model: popUpMenuModel,
+            child: const _PopUpMenuButton(),
+          ),
+        ),
+        popUpMenuModel.isPopUpMenuOpen
+            ? const OpacityModalWidget()
+            : const SizedBox.shrink(),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    popUpMenuModel.removeListener(_listener);
+  }
+}
+
+/// A widget used to make PopUp menu button with list of items.
+class _PopUpMenuButton extends StatelessWidget {
+  /// Creates a const [_PopUpMenuButton].
+  const _PopUpMenuButton({
+    super.key,
+  });
+
+  List<MenuItemButton> _makeItemButtons(ScreenStateModel? model) {
+    return <MenuItemButton>[
+      MenuItemButton(
+          onPressed: model?.requestFullscreen,
+          child: const Text('Enter fullscreen')),
+      MenuItemButton(
+          onPressed: model?.exitFullscreen,
+          child: const Text('Exit fullscreen')),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final popUpMenuModel = DataProvider.read<PopUpMenuModel>(context);
+    final screenStateModel = DataProvider.read<ScreenStateModel>(context);
+
+    return Container(
+      width: 100,
+      height: 100,
+      alignment: AlignmentDirectional.bottomEnd,
+      child: MenuAnchor(
+        builder:
+            (BuildContext context, MenuController controller, Widget? child) {
+          return IconButton(
+            onPressed: () {
+              controller.isOpen ? controller.close() : controller.open();
+            },
+            icon: const Icon(Icons.add),
+            tooltip: 'Select screen mode',
+          );
+        },
+        menuChildren: _makeItemButtons(screenStateModel),
+        alignmentOffset: const Offset(-80.0, 15.0),
+        onOpen: popUpMenuModel?.onToggelePopUpMenu,
+        onClose: popUpMenuModel?.onToggelePopUpMenu,
+      ),
+    );
+  }
+}
+
+/// A widget used to make Image from HTML, using tag <img>.
+class HtmlImageWidget extends StatelessWidget {
+  /// Creates a widget that builds Flutter widget tree from html.
+  const HtmlImageWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenStateModel = DataProvider.read<ScreenStateModel>(context);
+    final htmlWidgetModel = DataProvider.watch<HtmlWidgetModel>(context);
     return Expanded(
       child: AspectRatio(
         aspectRatio: 1,
-        child: GestureDetector(
-          onDoubleTap: () => onDoubleTapImage(),
-          child: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(12),
-            ),
+        child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: GestureDetector(
+            onDoubleTap: screenStateModel?.onDoubleTapImage,
             child: HtmlElementView(
               key: UniqueKey(),
               viewType: 'img-view',
-              creationParams: model?.imageUrl,
+              creationParams: htmlWidgetModel?.imageUrl,
             ),
           ),
         ),
@@ -211,15 +165,13 @@ class HtmlImageWidget extends StatelessWidget {
 /// A widget used to make URL string and button.
 class _UrlStringWidget extends StatelessWidget {
   /// Creates a const [_UrlStringWidget].
-  ///
-  /// The [onChanged] and [onGetImgBtnPressed] arguments must not be null.
   const _UrlStringWidget({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    final model = HtmlWidgetDataProvider.read(context);
+    final model = DataProvider.read<HtmlWidgetModel>(context);
     return Row(
       children: [
         Expanded(
--- lib/ui/widgets/home_screen/html_widget_data_provider.dart
+++ lib/ui/widgets/home_screen/html_widget_data_provider.dart
@@ -1,4 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_itsolutions_task/services/js_interop_service.dart';
+
+class ScreenStateModel extends ChangeNotifier {
+  /// Wether the Fullscreen mode is ON.
+  bool _isFullScreen = false;
+
+  /// Toggles full screen mode when double clicking on an image.
+  void onDoubleTapImage() {
+    _toggleFullScreen();
+  }
+
+  /// Toggles the screen between full screen and windowed modes.
+  void _toggleFullScreen() {
+    !_isFullScreen
+        ? JSInteropService.requestFullScreen()
+        : JSInteropService.exitFullScreen();
+
+    _isFullScreen = !_isFullScreen;
+  }
+
+  /// Sets the screen to full screen mode.
+  void requestFullscreen() {
+    if (_isFullScreen) return;
+    _toggleFullScreen();
+  }
+
+  /// Exit from full screen mode.
+  void exitFullscreen() {
+    if (!_isFullScreen) return;
+    _toggleFullScreen();
+  }
+}
 
 class HtmlWidgetModel extends ChangeNotifier {
   String? imageUrl; // https://pixlr.com/images/generator/text-to-image.webp
@@ -6,24 +38,34 @@ class HtmlWidgetModel extends ChangeNotifier {
   void updateImage() => notifyListeners();
 }
 
-class HtmlWidgetDataProvider extends InheritedNotifier<HtmlWidgetModel> {
-  const HtmlWidgetDataProvider({
-    required this.model,
+class PopUpMenuModel extends ChangeNotifier {
+  /// Wether the PopUp menu is appear.
+  bool _isPopUpMenuOpen = false;
+  bool get isPopUpMenuOpen => _isPopUpMenuOpen;
+
+  /// Updates the state when PopUp menu appears or disappears.
+  void onToggelePopUpMenu() {
+    _isPopUpMenuOpen = !_isPopUpMenuOpen;
+    notifyListeners();
+  }
+}
+
+class DataProvider<Model extends Listenable> extends InheritedNotifier<Model> {
+  const DataProvider({
+    required Model model,
     required super.child,
     super.key,
   }) : super(notifier: model);
 
-  final HtmlWidgetModel model;
-
-  static HtmlWidgetModel? watch(BuildContext context) {
+  static Model? watch<Model extends Listenable>(BuildContext context) {
     return context
-        .dependOnInheritedWidgetOfExactType<HtmlWidgetDataProvider>()
-        ?.model;
+        .dependOnInheritedWidgetOfExactType<DataProvider<Model>>()
+        ?.notifier;
   }
 
-  static HtmlWidgetModel? read(BuildContext context) {
+  static Model? read<Model extends Listenable>(BuildContext context) {
     return context
-        .getInheritedWidgetOfExactType<HtmlWidgetDataProvider>()
-        ?.model;
+        .getInheritedWidgetOfExactType<DataProvider<Model>>()
+        ?.notifier;
   }
 }
